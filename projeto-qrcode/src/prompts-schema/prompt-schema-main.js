4b76cc060cb3eff5daea958b3c8c225a0da519fa
Fernando Anisio <fernandodevlinux@gmail.com>
2025-01-07 19:16:52 -0300

    add-app-converter-coin-BRL-USD

--- projeto-qrcode/src/prompts-schema/prompt-schema-main.js
+++ projeto-qrcode/src/prompts-schema/prompt-schema-main.js
@@ -3,9 +3,7 @@ import chalk from "chalk";
 const promptSchemaMain = [
   {
     name: "select",
-    description: chalk.yellow.bold(
-      "Escolha a ferramenta (1 - QRCODE ou (2- PASSWORD"
-    ),
+    description: chalk.yellow.bold( "Escolha a ferramenta (1 - QRCODE ou (2- PASSWORD"),
     pattern: /^[1-2]+$/,
     message: chalk.red.italic("Escolha apenas entre 1 e 2"),
     required: true,
