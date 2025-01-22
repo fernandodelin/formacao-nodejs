868adc71e97da5f44119067af0bc75fd6b056d7b
Fernando Anisio <fernandodevlinux@gmail.com>
2025-01-22 00:49:43 -0300

    update-players-json

--- projeto-champions/src/app.ts
+++ projeto-champions/src/app.ts
@@ -9,8 +9,8 @@ function createApp() {
   app.use("/api", router);
 
   const corsOptions = {
-    origin: ["http://felipao.sistem.com", "http://gov.br"],
-    methods: ["GET", "UPDATE"],
+    origin: ["https://fernandodevlin.github.io"],
+    methods: ["GET", "UPDATE", "DELETE", "POST", "PATCH"],
   };
 
   app.use(cors());
